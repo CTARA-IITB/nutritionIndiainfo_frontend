b474878670e30d5dcdfff21f75320fcad9887b0e
vidya85 <sanapvidya4u@gmail.com>
2020-11-10 15:53:43 +0530

    legend added

--- frontend/src/components/Map/Map.jsx
+++ frontend/src/components/Map/Map.jsx
@@ -1,9 +1,10 @@
 import React,{useRef,useEffect} from 'react';
-import { geoMercator, geoPath, scaleSequential,interpolateRdYlGn, min,max,extent,select } from 'd3';
+import { geoMercator, format, geoPath, scaleQuantize, extent,select } from 'd3';
 import _ from 'lodash';
 import useResizeObserver from "../../useResizeObserver";
-
 import "./Map.css";
+import { legendColor } from 'd3-svg-legend'
+
 
 
 
@@ -49,33 +50,55 @@ useEffect(() => {
   const { width, height } = dimensions || wrapperRef.current.getBoundingClientRect();
   console.log("w&h",width, height);
   const projection = geoMercator().fitSize([width, height], geometry);
+  // const colorLegendG = svg.append('g').attr('transform', `translate(40,310)`);
 
   const pathGenerator = geoPath(projection);
   let mergedGeometry = addProperties(geometry.features,data);
-  let c1Value  = d => d.data_value;
+  console.log(mergedGeometry);
+   let c1Value  = d => d.data_value;
   let c2Value  = d => d.dataValue;
   
   let color_range = _.map(data, d =>{
     return +d.data_value
   });
   let [min,max] = extent(color_range);
-  let comp = (max - min)/3;
-  let low = min + comp;
-  let high = max - comp;
+  // let comp = (max - min)/3;
+  // let low = min + comp;
+  // let high = max - comp;
+  // console.log("minmax", min, max);
+  // console.log("lowhigh", low, high);
 
+  // let colorScale = scaleSequential().domain([max,min])
+  //     .interpolator(interpolateRdYlGn);
+    let colorScale = scaleQuantize().domain([min, max])
+      .range(["rgb(0, 128, 0)","rgb(255,255,0)", "rgb(255, 0, 0)"]);
+  // let colorScale = scaleOrdinal();
+  // colorScale.domain(c2Value)
+  //     .range("red","yellow", "green");
+  
+  
+  // let colorScale = (v) =>{
+  //   if (typeof v != "undefined") {
+  //       let selectedColor;
+  //         if (v < low) {selectedColor =  "#24562B";}//matte green
+  //         else if (v >= low && v <= high) {selectedColor =  "#FFE338";}//matte yellow
+  //         else if (v > high) {selectedColor =  "#B2022F";} //matte red
+  //       return selectedColor;
+  //   }
+  //   else {
+  //     return "#A9A9B0";
+  //   }
+  // };
 
-  let colorScale = (v) =>{
-    if (typeof v != "undefined") {
-        let selectedColor;
-          if (v < low) {selectedColor =  "#24562B";}//matte green
-          else if (v >= low && v <= high) {selectedColor =  "#FFE338";}//matte yellow
-          else if (v > high) {selectedColor =  "#B2022F";} //matte red
-        return selectedColor;
-    }
-    else {
-      return "#A9A9B0";
-    }
-  };
+ 
+
+  // colorLegendG.call(colorLegend, {
+  //   colorScale,
+  //   circleRadius: 8,
+  //   spacing: 20,
+  //   textOffset: 12,
+  //   backgroundRectWidth: 235
+  // });
 
   //OnMouseOver
 
@@ -87,6 +110,8 @@ useEffect(() => {
       .style("top",  event.clientY - 30 + "px");	
     }
   };
+
+ 
 
   svg
     .selectAll(".polygon")
@@ -100,6 +125,20 @@ useEffect(() => {
       .duration(500)    
       .style("opacity", 0); 
     });
+
+    svg.append("g")
+  .attr("class", "legendQuant")
+  .attr("transform", "translate(20,20)");
+
+  let legend = legendColor()
+    .labelFormat(format(".2f"))
+    .title("Legend")
+    .titleWidth(100)
+    .scale(colorScale);
+
+  svg.select(".legendQuant")
+    .call(legend);
+    
 }, [geometry, dimensions, data])
 
 

4634ddcb524122a58c15939ede9eda0e38275e3f
vidya85 <sanapvidya4u@gmail.com>
2020-12-15 22:55:40 +0530

    'missinurls.py'

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include  
-from dashboard.views import IndicatorListView, SubgroupListView, TimeperiodListView, AreaListView, IndiaMapView, AreaDataView, AreaMapView, GetUnitView  ,GetUnitNameView                              
+from dashboard.views import IndicatorListView, SubgroupListView, IndicatorDetailView, TimeperiodListView, AreaListView, IndiaMapView, AreaDataView, AreaMapView, GetUnitView  ,GetUnitNameView                              
 
 
 urlpatterns = [
@@ -12,6 +12,6 @@ urlpatterns = [
     path('api/areaData/<int:indicator>/<int:subgroup>/<int:timeperiod>/<int:area>', AreaDataView.as_view(), name='areaData'),
     path('api/areaMap/<int:area>', AreaMapView.as_view(), name='areaMap'),
     path('api/getUnit/<int:indicator>/<int:subgroup>', GetUnitView.as_view(), name='unit'),
-    path('api/getUnitName', GetUnitNameView.as_view(), name='unit')
+    path('api/getUnitName', GetUnitNameView.as_view(), name='unit'),
     path('api/getIndicatorDetails/<int:tab>', IndicatorDetailView.as_view(), name='indicatorDetails')
 ]

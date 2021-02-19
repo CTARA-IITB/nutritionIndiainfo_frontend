0b01fc697af856a0184febadb4bfade5744f0ef4
AnimeshN <animesh.nauti@gmail.com>
2021-02-19 12:50:06 +0530

    district updated

--- frontend/src/containers/UseData.js
+++ frontend/src/containers/UseData.js
@@ -3,7 +3,7 @@ import { json } from 'd3';
 import { feature } from 'topojson';
 
 const jsonIndianstate =
-    'https://gist.githubusercontent.com/AnimeshN/da2cb5d197bdc0f811a2cda3eaf2e97d/raw/india_state_nfhs4.topojson';;
+    'https://gist.githubusercontent.com/AnimeshN/b705d4f1a910a16013348e6743d597f4/raw/13ff136e1cd3a90b2fcca370158960dddf74fbee/india_state_old.json';;
 const jsonIndiaDistrict = 'https://gist.githubusercontent.com/AnimeshN/7dd993301b4fb2657a221e3f5e148069/raw/india_district_nfhs4.topojson';
 
 export const useData = (selArea) => {
@@ -12,7 +12,8 @@ export const useData = (selArea) => {
     useEffect(() => {
     // const bundle = {}
     json(jsonIndianstate).then(stateTopology => {
-      const state = stateTopology.objects.india;
+      console.log("JSONINDIASTATE",stateTopology)
+      const state = stateTopology.objects.india_state_old;
       json(jsonIndiaDistrict).then(districtTopology =>{
       	const dist = districtTopology.objects.india;
       	setData({'state':feature(stateTopology,state),'dist':feature(districtTopology,dist)})
@@ -38,7 +39,7 @@ export const useData = (selArea) => {
 
   export const useDataState =(areaName,indiaDistrictGeojson) =>{
     let [data, setData] = useState(null);
-    areaName = (areaName === "Telangana")?"Telengana":areaName; // hack.. will later change areaname in geojson data to Telangana
+    // areaName = (areaName === "Telangana")?"Telengana":areaName; // hack.. will later change areaname in geojson data to Telangana
   
     useEffect(()=>{
       let features;
@@ -55,7 +56,7 @@ export const useData = (selArea) => {
   }
   
 const jsonNewIndianstate = 'https://gist.githubusercontent.com/AnimeshN/8ee87a2d19b3683253faaa27b168250b/raw/8c978e5914365982438314ded456db27e2008736/india_state_updated_topojsonv1.json';
-const jsonNewIndiaDistrict = 'https://gist.githubusercontent.com/AnimeshN/9112826520d38c6091733bc8abc1e280/raw/96fa05a0f6874741416e521b4e49c5aed0c5d2da/ind_dist_nfhs5_topo_v2.json'
+const jsonNewIndiaDistrict = 'https://gist.githubusercontent.com/AnimeshN/2ccb4212bc27490b5ef6a06d5d6461d9/raw/e014a11d3f11b51e4842f0105646a6bfb9494b99/india_dist_v2.json'
 
 export const useNewBoundaries = () => {
     let [data, setData] = useState(null);
@@ -63,7 +64,7 @@ export const useNewBoundaries = () => {
     json(jsonNewIndianstate).then(stateTopology => {
       const state = stateTopology.objects.state
       json(jsonNewIndiaDistrict).then(districtTopology =>{
-        const dist = districtTopology.objects.ind_dist_nfhs5;
+        const dist = districtTopology.objects.india_dist_v2;
         let new_state = feature(stateTopology,state);
         let new_dist = feature(districtTopology,dist);
       	setData({'new_state': new_state,'new_dist': new_dist})
@@ -78,7 +79,7 @@ export const useNewBoundaries = () => {
     let [data, setData] = useState(null);
     useEffect( async () => {
     const distTopo = await json(jsonNewIndiaDistrict)
-      setData({'dist':feature(distTopo,distTopo.objects.ind_dist_nfhs5)})
+      setData({'dist':feature(distTopo,distTopo.objects.india_dist_v2)})
   }, []);
   return data;
   }

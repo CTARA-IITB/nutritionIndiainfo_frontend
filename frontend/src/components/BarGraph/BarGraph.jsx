f8f2442e0d4fe121a3f57a4776c4b02489a37265
jyoti <jyotigpt790@gmail.com>
2021-03-27 11:14:53 +0530

    bar chart2

--- frontend/src/components/BarGraph/BarGraph.jsx
+++ frontend/src/components/BarGraph/BarGraph.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import {Bar} from 'react-chartjs-2';
 import { json } from 'd3';
 
-export const BarGraph = ({indicatorBar, graphTitle, 
+const BarGraph = ({indicatorBar, graphTitle, 
   graphTimeperiod, graphUnit, areaName}) => { 
       
     let barLabel=[];
@@ -41,7 +41,7 @@ export const BarGraph = ({indicatorBar, graphTitle,
         datab = {
               labels:barLabel,
               datasets: [{
-              label: [graphTitle, graphUnit],
+              label: [graphTitle, graphUnit,graphTimeperiod],
               data:barData,
               xAxisID:'xAxis1',
               //backgroundColor: "rgb(255, 99, 132)"
@@ -62,12 +62,14 @@ export const BarGraph = ({indicatorBar, graphTitle,
               display: true,
               text: [graphTitle +','+ graphUnit, areaName +', '+ graphTimeperiod],
               fontColor: "black",
-          },
+            },
             scales: {
               xAxes:[{
                 id:'xAxis1',
                 type:"category",
                 ticks:{
+                  fontSize: 11,
+                  fontColor: "black",
                   callback:function(label){
                     var subgroup = label.split(";")[0];
                     return subgroup;
@@ -85,6 +87,8 @@ export const BarGraph = ({indicatorBar, graphTitle,
                   drawOnChartArea: false, // only want the grid lines for one axis to show up
                 },
                 ticks:{
+                  fontSize: 9,
+                  fontColor:"black",
                   minRotation: 0,
                   callback:function(label){
                     var subgroup = label.split(";")[0];
@@ -102,6 +106,8 @@ export const BarGraph = ({indicatorBar, graphTitle,
               }],
               yAxes: [{
                 ticks: {
+                  fontSize: 8,
+                  fontColor:"black",
                   beginAtZero: true
                 },
                 gridLines: {
@@ -113,3 +119,143 @@ export const BarGraph = ({indicatorBar, graphTitle,
         />    
         )
      }
+   
+  const BarGraphArea = ({indicatorBar, graphTitle, 
+      graphTimeperiod, graphUnit,selIndiaData,level,
+      unit,unitName,selArea,selIndicator,indicatorSense, 
+      isLevelThree,selSubgroup,selTimeperiod,areaName,
+      selStateData}) => { 
+
+        let barLabel=[];
+        let barData=[];
+        let datab = [];
+        let stateDataValue,stateAreaName;
+        let color;
+
+        if(selIndiaData && level=="1")
+          {
+            selIndiaData.map(i=>{
+              barLabel.push(i.area.area_name)
+              barData.push(+i.data_value)
+              color= 'rgb(142, 209, 25)'
+             })
+          }        
+        if(selStateData && level=="2")
+          {
+            for(let j=0;j<selIndiaData.length;j++){
+              if(+selArea===selIndiaData[j].area.area_id)
+              { 
+                stateDataValue=selIndiaData[j].data_value
+                stateAreaName=selIndiaData[j].area.area_name
+                barLabel.push(stateAreaName)
+                barData.push(stateDataValue)
+              }           
+            }
+            selStateData.map(i=>{
+              barLabel.push(i.area.area_name)
+              barData.push(+i.data_value)
+              color=' rgb(142, 209, 25)'
+            })
+          }           
+          if(level==="3")
+          {
+            console.log(selStateData[0].area.area_name,"state,distrrr")
+            // for(let j=0;j<selIndiaData.length;j++){
+            //   if(+selArea===selIndiaData[j].area.area_id)
+            //   { 
+            //     stateDataValue=selIndiaData[j].data_value
+            //     stateAreaName=selIndiaData[j].area.area_name
+            //     barLabel.push(stateAreaName)
+            //     barData.push(stateDataValue)
+            //     color='rbg(114, 54, 117)'
+            //   }           
+            // }
+            // selStateData.map(i=>{
+            //   barLabel.push(i.area.area_name)
+            //   barData.push(+i.data_value)
+ 
+            // })
+          }              
+ 
+            datab = {
+                  labels:barLabel,
+                  datasets: [{
+                  label: [graphTitle, graphUnit, graphTimeperiod],
+                  data:barData,
+                  xAxisID:'xAxis1',
+                  //backgroundColor: "rgb(255, 99, 132)"
+                  backgroundColor:color,
+                  borderColor: "rgb(142, 209, 25)",
+                  borderWidth: 1
+                  }]
+            }
+        
+        
+        return (
+            <Bar data={datab}  options={{
+                legend:
+                {
+                  display: false,
+                },
+                title: {
+                  display: true,
+                  text: [graphTitle +','+ graphUnit, areaName +','+ graphTimeperiod],
+                  fontColor: "black",
+                },
+                scales: {
+                  xAxes:[{
+                    id:'xAxis1',
+                    type:"category",
+                    ticks:{
+                      fontSize: 11,
+                      fontColor: "black",
+                      callback:function(label){
+                        var subgroup = label.split(";")[0];
+                        return subgroup;
+                      }
+                    },
+                    gridLines: {
+                      drawOnChartArea: false, // only want the grid lines for one axis to show up
+                    },
+      
+                  },
+                  {
+                    id:'xAxis2',
+                    type:"category",
+                    gridLines: {
+                      drawOnChartArea: false, // only want the grid lines for one axis to show up
+                    },
+                    ticks:{
+                      fontSize: 7,
+                      fontColor:"black",
+                      minRotation: 0,
+                      callback:function(label){
+                        var subgroup = label.split(";")[0];
+                        var type = label.split(";")[1];
+                        if(subgroup === "Rural" || subgroup === "Female"  || subgroup ==="ST" || subgroup === "Middle"){
+                          return type
+                        }
+                        else
+                        return ""
+                      }  
+                    },
+                    gridLines: {
+                      drawOnChartArea:false
+                  }
+                  }],
+                  yAxes: [{
+                    ticks: {
+                      fontSize: 7,
+                      fontColor:"black",
+                      beginAtZero: true
+                    },
+                    gridLines: {
+                      drawOnChartArea:false
+                  }
+                  }]
+                }
+              }}
+            />    
+            )
+         }
+         export { BarGraph, BarGraphArea }

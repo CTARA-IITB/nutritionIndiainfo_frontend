8c5dfe471ab92d3c287251e4e8e51ca154a43423
AnimeshN <animesh.nauti@gmail.com>
2021-04-30 14:59:00 +0530

    map aligned with heading and toggle button added

--- frontend/src/components/Trend/Trend.jsx
+++ frontend/src/components/Trend/Trend.jsx
@@ -16,13 +16,12 @@ const height = window.screen.height/2;
 const tickLength = 8;
 const margin = {
   left: 100,
-  top: 50,
+  top: 80,
   right: 50,
   bottom: 100,
 };
 export const Trend = ({indicatorTrend, graphTitle, graphSubgroup, graphUnit, areaName, toggleStateBurden}) => { 
   const [data, setData] = useState(null);
-
   const parseTime = timeParse('%d %b %y');
 	 let tooltip2 = select("body").append("div")
     .attr("class", "tooltip2")
@@ -59,7 +58,7 @@ export const Trend = ({indicatorTrend, graphTitle, graphSubgroup, graphUnit, are
   let min_year = min_d.getFullYear();
   let min_month = min_d.getMonth();
   let min_day = min_d.getDate();
-  let min_date = new Date(min_year, min_month-3, min_day);
+  let min_date = new Date(min_year-1, min_month, min_day);
   
   let max_d =  max(listofDate);
   let max_year = max_d.getFullYear();
@@ -71,12 +70,17 @@ export const Trend = ({indicatorTrend, graphTitle, graphSubgroup, graphUnit, are
   .domain([min_date, max_date])
   .range([0, innerWidth]);
 
+const xValue = d => d.middle_date;
+let yValue;
+if(toggleStateBurden)
+  yValue = d => d.data_value;
+else
+  yValue = d =>d.data_value_num;
 const yScale = scaleLinear()
-  .domain([0, max(data, (d) => d.data_value)])
+  .domain([0, max(data, d=>yValue(d))])
   .range([innerHeight, 0]);
 
-const xValue = d => d.middle_date;
-const yValue = d => d.data_value;
+
 
 const lineGenerator = line()
 .x(d => xScale(xValue(d)))
@@ -102,18 +106,19 @@ let yAxis = (
   ></line>)
 
   let heading = (
-    <text x={width/2 -200} style={{ textAnchor: 'start',fontSize:"13",fontWeight:"bold" }}> 
+    <text x={width/2 -150} y={50}style={{ textAnchor: 'start',fontSize:"13",fontWeight:"bold" }}> 
     
     {graphTitle},  {graphUnit}, {areaName}</text>
   )
     
   return (
     <svg width={width} height={height}>
+        {heading}
+
       <g
         transform={`translate(${margin.left},${margin.top})`}
       >
         <path d={lineGenerator(data)}></path>							// Draw line Graph
-        {heading}
         
         {xAxis} 					//Draw x axis line
         {yAxis} 					//Draw y axis line
@@ -161,25 +166,25 @@ let yAxis = (
           <g key={d.timeperiod}>
           <rect
             fill="rgba(142,209,26,.5)"				
-            y={yScale(d.data_value)}
+            y={yScale(yValue(d))}
             x={xScale(d.start_date)}
-            height={yScale(0) - yScale(d.data_value)}
+            height={yScale(0) - yScale(yValue(d))}
             width={
               xScale(d.end_date) - xScale(d.start_date)
             }
             
             onMouseOver={() =>{
         			tooltip2.transition().duration(500).style("opacity", 1);
-              tooltip2.html(`${d.timeperiod}:${d.data_value}</br>start date:${formatTooltipTime(d.start_date)}</br>end date:${formatTooltipTime(d.end_date)}</div>`)
+              tooltip2.html(`${d.timeperiod}:${yValue(d)}</br>start date:${formatTooltipTime(d.start_date)}</br>end date:${formatTooltipTime(d.end_date)}</div>`)
           		.style("left", xScale(d.middle_date) + 1000 + "px")
-          		.style("top", yScale(d.data_value) + 250+"px")
+          		.style("top", yScale(yValue(d)) + 250+"px")
              ;
               }}
             
             onMouseOut={()=>{tooltip2.transition().duration(500).style("opacity", 0)}}
           />
             <text x={xScale(d.middle_date)} 							//lable on top of bar
-                 	y={yScale(d.data_value)}							
+                 	y={yScale(yValue(d))}							
               		style={{ textAnchor: 'middle',fontSize:'12' }}
               		dy="-.2em"
               >{d.timeperiod}</text>

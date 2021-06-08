26a4e298cbaa2b441486ed961ecdfdb0158275b2
vidya85 <sanapvidya4u@gmail.com>
2021-06-08 16:33:10 +0530

    'thirbargraphnodatamsg'

--- frontend/src/components/Bar/BarAreaComponent.jsx
+++ frontend/src/components/Bar/BarAreaComponent.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import {HorizontalBar} from 'react-chartjs-2';
+import "./BarArea.css";
 
 class BarAreaComponent extends React.PureComponent {
 
   componentRef = React.createRef();
   render() {
+    console.log("data", this.props.data.labels.length);
+    if(this.props.data.labels.length > 0)
+    {
     return (
         <div>
             <HorizontalBar ref={this.componentRef} height={this.props.calculatedHeight} id="BarArea"  data={this.props.data} options={this.props.options}/>
         </div>
     );
+    }
+    else{
+      return (
+        <div id="statMsg">No data: please select another survey</div>
+    );
+    }
   }
 }
 export default BarAreaComponent;

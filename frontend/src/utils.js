f3265f0d449d9571b02187ac5ee4e53b9fd751a4
vidya85 <sanapvidya4u@gmail.com>
2021-06-12 17:09:49 +0530

    'removeconsole'

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -51,13 +51,11 @@ export const createHierarchy = (options) =>{
     const url_1 =  await fetch(`http://nutritionindia.communitygis.net:8983/solr/nutritionv11/select?fl=timeperiod_id%2Ctimeperiod%2Cunit_id%2Cunit_name%2Cdata_value%2Cdata_value_num%2Csubgroup_id%2Csubgroup_name%2Csubgroup_category%2Cstart_date%2Cend_date&fq=area_id%3A${area}&fq=indicator_id%3A${indicator}&fq=subgroup_id%3A6&omitHeader=true&q=*%3A*&rows=400&sort=timeperiod_id%20asc`);
     const body_1 = await url_1.json();
     setIndicatorTrend(body_1.response.docs)
-    console.log("ius", indicator, area, timeperiod);
 
 
    // const url_2 = await fetch(`http://localhost:8000/api/getIndicatorBar/${indicator}/${timeperiod}/${area}`);
     const url_2 = await fetch(`http://nutritionindia.communitygis.net:8983/solr/nutritionv11/select?fl=unit_id%2Cunit_name%2Csubgroup_name%2Csub_category%2Cdata_value%2Cdata_value_num%2Csubgroup_id%2Csubgroup_name_subgroup_category&fq=area_id%3A${area}&fq=indicator_id%3A${indicator}&fq=timeperiod_id%3A${timeperiod}&omitHeader=true&q=*%3A*&rows=100&sort=subgroup_order%20asc`);
     const body_2 = await url_2.json();
-    console.log("body_2",)
     setIndicatorBar(body_2.response.docs);
 
     // if (level === 1)

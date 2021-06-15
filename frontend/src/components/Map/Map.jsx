e15336e53ccb7e4df47c17dc175db41efa170efd
Shweta-tech <shwetapanwar97@gmail.com>
2021-06-15 20:30:36 +0530

    legend issue resolved

--- frontend/src/components/Map/Map.jsx
+++ frontend/src/components/Map/Map.jsx
@@ -95,7 +95,7 @@ export const Map = ({
   //merge geometry and data
 
   function addProperties(geojson, data) {
-    console.log('DDAATTAA',data)
+   
     let newArr = _.map(data, function (item) {
       return {
         areacode: item.area_code,
@@ -269,14 +269,14 @@ export const Map = ({
       medium = 10.0;
       high = 15.0;
       highest = 20.0
-    sampleCategoricalData = ["<5%", "5-10%", "10-15%", "15-20%", ">20%", "No Data"]
+      sampleCategoricalData = ["<5%", "5-10%", "10-15%", "15-20%", ">20%", "No Data"]
 
     } else if (selIndicator == 17 || selIndicator == 18 || selIndicator == 12 || selIndicator == 13) {
        low = 10.0;
        medium = 20.0;
        high = 30.0;
        highest = 40.0;
-    sampleCategoricalData = ["<10%", "10-20%", "20-30%", "30-40%", ">40%", "No Data"]
+      sampleCategoricalData = ["<10%", "10-20%", "20-30%", "30-40%", ">40%", "No Data"]
 
     } else if(selIndicator == 71 || selIndicator == 26 )
     {
@@ -284,7 +284,7 @@ export const Map = ({
       medium = 20.0;
       high = 40.0;
       highest = 60.0;
-    sampleCategoricalData = ["<5%", "5-20%", "20-40%", "40-60%", ">60%", "No Data"]
+      sampleCategoricalData = ["<5%", "5-20%", "20-40%", "40-60%", ">60%", "No Data"]
 
     } else if(selIndicator == 20 || selIndicator == 108)
     {
@@ -292,7 +292,7 @@ export const Map = ({
       medium = 2.0;
       high = 5.0;
       highest = 10.0;
-    sampleCategoricalData = ["<1%", "1-2%", "2-5%", "5-10%", ">10%", "No Data"]
+      sampleCategoricalData = ["<1%", "1-2%", "2-5%", "5-10%", ">10%", "No Data"]
 
      }else if(selIndicator == 107)
     {
@@ -300,7 +300,7 @@ export const Map = ({
       medium = 0.5;
       high = 1;
       highest = 2.5;
-    sampleCategoricalData = ["<0.1%", "0.1-0.5%", "0.5-1%", "1-2.5%", ">2.5%", "No Data"]
+      sampleCategoricalData = ["<0.1%", "0.1-0.5%", "0.5-1%", "1-2.5%", ">2.5%", "No Data"]
 
     }else if(selIndicator == 89)
     {
@@ -308,7 +308,7 @@ export const Map = ({
       medium = 10.0;
       high = 20.0;
       highest = 30.0;
-    sampleCategoricalData = ["<5%", "5-10%", "10-20%", "20-30%", ">30%", "No Data"]
+      sampleCategoricalData = ["<5%", "5-10%", "10-20%", "20-30%", ">30%", "No Data"]
 
     }else if(arr20to80.includes(selIndicator))
     {
@@ -316,13 +316,13 @@ export const Map = ({
       medium = 40.0;
       high = 60.0;
       highest = 80.0;
-    sampleCategoricalData = ["<20%", "20-40%", "40-60%", "60-80%", ">80%", "No Data"]
+      sampleCategoricalData = ["<20%", "20-40%", "40-60%", "60-80%", ">80%", "No Data"]
 
     }
     
   
-    let colorScale,colorScale_new;
-  
+    let colorScale;
+  let colorScale_new;
     let colorScale2;
     let arrsuw = [19,21,17,18,12,13,71,26,20,108,107,89,31,11,28,6,7,37,51,42,84]; 
     if (unit == 1  && toggleStateBurden == true)
@@ -331,14 +331,21 @@ export const Map = ({
     {
     colorScale2 = scaleThreshold().domain([low, medium, high, highest])
     .range(["#8e0000", "#fe0000", "#ffc000", "#ffff00", "#00af50"]); 
-   colorScale_new = scaleOrdinal().domain(sampleCategoricalData)
+    if(typeof sampleCategoricalData != 'undefined'){
+      colorScale_new = scaleOrdinal().domain(sampleCategoricalData)
       .range(["#8e0000", "#fe0000", "#ffc000", "#ffff00", "#00af50","#A9A9B0"]);
+    }
     }
     else{
       colorScale2 = scaleThreshold().domain([low, medium, high, highest])
     .range(["#00af50", "#ffff00", "#ffc000", "#fe0000", "#8e0000"]); 
-    colorScale_new = scaleOrdinal().domain(sampleCategoricalData)
+    console.log(sampleCategoricalData)
+      if(typeof sampleCategoricalData != 'undefined'){
+      colorScale_new = scaleOrdinal().domain(sampleCategoricalData)
       .range(["#00af50", "#ffff00", "#ffc000", "#fe0000", "#8e0000","#A9A9B0"]);
+      }
+
+  
     }
 
     let colorScale4 = scaleQuantize()
@@ -355,9 +362,11 @@ export const Map = ({
     }
     else if (indicatorSense === 'Negative') {
       colorScale = colorScale4;
+      colorScale_new = colorScale4;
 
     } else if (indicatorSense === 'Positive') {
       colorScale = colorScale4_p;
+      colorScale_new = colorScale4_p;
 
     }
     

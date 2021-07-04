ca1dd75d013d7d708156dfbd807dd42e99671dc0
vermakriti <kriti.verma.it@gmail.com>
2021-07-04 15:12:45 +0530

    minor

--- frontend/src/components/SkeletonCard.js
+++ frontend/src/components/SkeletonCard.js
@@ -11,41 +11,41 @@ import {
 } from '@material-ui/core';
 
 const SkeletonDropdown = () => {
-    return (
-      <Box  p={2} lg={12}>
-        <Grid container direction="row" justify="space-between" alignItems="flex-end">
-          <Grid  xs={1}>
-            <Skeleton height={25}/>
-          </Grid>  
-          <Grid  xs={3}>
-            <Skeleton height={25}/>
-          </Grid>  
-          <Grid  xs={1}>
-            <Skeleton height={25}/>
-          </Grid>
-        </Grid>
-        <Box pt={7} lg={12}>
-        <Grid  lg={12} item container spacing={2} padding={2} >
-          <Grid lg={12} sm={6}>
-            <Row className=' mt-3 mb-3'>
-              <Col><Skeleton circle={true}  width ={65} height={65}/> </Col>
-              <Col><Skeleton height={30}/> </Col>
-              <Col><Skeleton height={30}/> </Col>
-              <Col><Skeleton height={30}/> </Col>
-              <Col><Skeleton height={30}/> </Col>
-              <Col><Skeleton height={30}/> </Col>
-              <Col><Skeleton circle={true}  width ={30} height={30}/> </Col>
-            </Row>
-          </Grid>
-          <Grid item lg={6} xs={12} sm={6}  zeroMinWidth><Skeleton height={400}/></Grid>
-          <Grid item lg={6} xs={12} sm={6}  ><Skeleton height={400}/></Grid>
-          <Grid item lg={6} xs={12} sm={6}  ><Skeleton height={400}/></Grid>
-          <Grid item lg={6} xs={12} sm={6}  ><Skeleton height={400}/></Grid>
+  return (
+    <Box  p={2} lg={12}>
+      <Grid container direction="row" justify="space-between" alignItems="flex-end">
+        <Grid  xs={1}>
+          <Skeleton height={25}/>
         </Grid>  
-        </Box>
+        <Grid  xs={3}>
+          <Skeleton height={25}/>
+        </Grid>  
+        <Grid  xs={1}>
+          <Skeleton height={25}/>
+        </Grid>
+      </Grid>
+      <Box pt={7} lg={12}>
+      <Grid  lg={12} item container spacing={2} padding={2} >
+        <Grid lg={12} sm={6}>
+          <Row className=' mt-3 mb-3'>
+            <Col><Skeleton circle={true}  width ={65} height={65}/> </Col>
+            <Col><Skeleton height={30}/> </Col>
+            <Col><Skeleton height={30}/> </Col>
+            <Col><Skeleton height={30}/> </Col>
+            <Col><Skeleton height={30}/> </Col>
+            <Col><Skeleton height={30}/> </Col>
+            <Col><Skeleton circle={true}  width ={30} height={30}/> </Col>
+          </Row>
+        </Grid>
+        <Grid item lg={6} xs={12} sm={6}  zeroMinWidth><Skeleton height={400}/></Grid>
+        <Grid item lg={6} xs={12} sm={6}  ><Skeleton height={400}/></Grid>
+        <Grid item lg={6} xs={12} sm={6}  ><Skeleton height={400}/></Grid>
+        <Grid item lg={6} xs={12} sm={6}  ><Skeleton height={400}/></Grid>
+      </Grid>  
       </Box>
-    );
-  };
+    </Box>
+  );
+};
   const SkeletonMapCard = () => {
 
     return (
@@ -57,20 +57,13 @@ const SkeletonDropdown = () => {
       </section>
     );
   };
-  const SkeletonCard = () => {
-
-    return (
-      <section>
-        <div>
-        <Skeleton rectangle={true} height={250} width={240} />&nbsp;&nbsp;&nbsp;&nbsp;
-        <Skeleton rectangle={true} height={250} width={240} />
-        <br/>
-        <br/>
-        <Skeleton rectangle={true} height={250} width={240} />&nbsp;&nbsp;&nbsp;&nbsp;
-        <Skeleton rectangle={true} height={250} width={240} />
-      </div>
-      </section>
-
-);
-  };
-   export { SkeletonCard, SkeletonMapCard, SkeletonDropdown}
+const SkeletonCard = () => {
+  return (
+    <Box lg={12}>
+      <Grid  lg={12} item container>
+        <Grid item lg={12} xs={12} sm={12}><Skeleton height={400}/></Grid>
+      </Grid>
+    </Box>
+  );
+};
+export { SkeletonCard, SkeletonMapCard, SkeletonDropdown}

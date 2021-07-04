e0838e210e2a6391b85c94abe47d907ed311cf51
vermakriti <kriti.verma.it@gmail.com>
2021-07-04 12:50:11 +0530

    skeleton

--- frontend/src/components/SkeletonCard.js
+++ frontend/src/components/SkeletonCard.js
@@ -1,35 +1,33 @@
 import React from "react";
 import "./SkeletonCard.css";
 import Skeleton from "react-loading-skeleton";
-import {Row } from 'react-bootstrap';
+import {Row,Col} from 'react-bootstrap';
 const SkeletonDropdown = () => {
 
     return (
       <section>
+        {/* <Row >
+          <Col md={2}><Skeleton duration={.5} height={40} width={150} /> </Col>
+          <Col md={{ span: 2, offset: 3 }}><Skeleton duration={.15} height={60} width={65} /> </Col>
+        </Row> */}
+        {/* <Row>
+          <Col md={{ span: 6, offset: 3 }}><Skeleton duration={.15} height={20} width={300} /></Col>
+        </Row> */}
         <Row>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<h2 className="section-title" >
-          <Skeleton duration={.15} height={20} width={300} />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        </h2>
-        <hr/>
-        <h2 className="section-title">
-          <Skeleton duration={.15} height={20} width={300} />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        </h2>
-        <hr/>
-        <h2 className="section-title">
-          <Skeleton duration={.15} height={20} width={300} />
-        </h2>
-        <hr/>
-         </Row>
-         <Row>
-         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<Skeleton rectangle={true} height={400} width={600} />
-        &nbsp;<Skeleton rectangle={true} height={400} width={600} /> 
-        {/* &nbsp;&nbsp;<Skeleton rectangle={true} height={100} width={100} /> */}
+          <Col padding={2}><Skeleton circle={true} width={65} height={65} /><Skeleton duration={.15} height={20} width={100} /> </Col>
+          <Col> <Skeleton duration={.5} height={20} width={100} /></Col>
+          <Col><Skeleton duration={.5} height={20} width={100} /> </Col>
+          <Col> <Skeleton duration={.5} height={20} width={100} /></Col>
+          <Col><Skeleton duration={.5} height={20} width={100} /> </Col>
+        </Row>
+        <Row >
+          <Col><Skeleton rectangle={true} height={400} width={600} /></Col>
+          <Col><Skeleton rectangle={true} height={400} width={600} /> </Col>
         </Row>   
         <Row>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<Skeleton rectangle={true} height={400} width={600} />
-        &nbsp;<Skeleton rectangle={true} height={400} width={600} /> 
-        {/* &nbsp;&nbsp;<Skeleton rectangle={true} height={100} width={100} /> */}
-        </Row>   
+          <Col><Skeleton rectangle={true} height={400} width={600} /></Col>
+          <Col><Skeleton rectangle={true} height={400} width={600} /> </Col>
+        </Row>  
        </section>
     );
   };

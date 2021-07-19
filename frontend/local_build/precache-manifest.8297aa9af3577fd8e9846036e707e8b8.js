76a85a931d4b82a97dfa53506511d844ffc4e775
AnimeshN <animesh.nauti@gmail.com>
2021-07-19 11:14:42 +0530

    del

---
+++ frontend/local_build/precache-manifest.8297aa9af3577fd8e9846036e707e8b8.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67951581188dbaded5b0a62efe6e30c0",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "d28312ada2141a20d7a3",
+    "url": "/dashboard/static/css/3.fece827b.chunk.css"
+  },
+  {
+    "revision": "68217db135cfe07da4c8",
+    "url": "/dashboard/static/css/main.b9330f87.chunk.css"
+  },
+  {
+    "revision": "03627c8ad9adb19a560a",
+    "url": "/dashboard/static/js/2.076a26f6.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/dashboard/static/js/2.076a26f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d28312ada2141a20d7a3",
+    "url": "/dashboard/static/js/3.f9318a2a.chunk.js"
+  },
+  {
+    "revision": "f659f804ac6e8f79449f4d2ee38b34f8",
+    "url": "/dashboard/static/js/3.f9318a2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91423a074f0e1b4365a5",
+    "url": "/dashboard/static/js/4.9bbe8c61.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/dashboard/static/js/4.9bbe8c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac5856f9783fe96ee50a",
+    "url": "/dashboard/static/js/5.2427e321.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/dashboard/static/js/5.2427e321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68217db135cfe07da4c8",
+    "url": "/dashboard/static/js/main.dd2e832b.chunk.js"
+  },
+  {
+    "revision": "28464d1646e9ef38f023",
+    "url": "/dashboard/static/js/runtime-main.902b84c1.js"
+  },
+  {
+    "revision": "0f8f64bd7c60e3aa9e0aa66575428372",
+    "url": "/dashboard/static/media/Adolescene.0f8f64bd.png"
+  },
+  {
+    "revision": "ba4036c3d7010ad89c8f02a7c301f74c",
+    "url": "/dashboard/static/media/DeliveryPNC.ba4036c3.png"
+  },
+  {
+    "revision": "bcd924316284daa249efc465c3b57b62",
+    "url": "/dashboard/static/media/Pregnancy.bcd92431.png"
+  },
+  {
+    "revision": "943ea96bf269e26e13dc1ffd90ea46d9",
+    "url": "/dashboard/static/media/Women-of-Reproductive-Age.943ea96b.png"
+  },
+  {
+    "revision": "77dd4cf30e8bb6b712d521efd44820ef",
+    "url": "/dashboard/static/media/ctara-logo.77dd4cf3.png"
+  },
+  {
+    "revision": "06109a052250d43bc272101fca8eb6d9",
+    "url": "/dashboard/static/media/i-con5.06109a05.png"
+  },
+  {
+    "revision": "6b049af7d18f7736e56dbd6d9c585cb2",
+    "url": "/dashboard/static/media/nutrition-logo-footer.6b049af7.svg"
+  },
+  {
+    "revision": "6b049af7d18f7736e56dbd6d9c585cb2",
+    "url": "/dashboard/static/media/nutrition-logo.6b049af7.svg"
+  },
+  {
+    "revision": "5118c7a854e0bd4d114d11d2b593c381",
+    "url": "/dashboard/static/media/pa.5118c7a8.png"
+  },
+  {
+    "revision": "eca4bb0b46025258c6caf5fa989434ca",
+    "url": "/dashboard/static/media/proditech-logo.eca4bb0b.png"
+  }
+]);

bb5747339d1d43b027ea9dc46b9dcc35336666d0
AnimeshN <animesh.nauti@gmail.com>
2021-08-10 10:16:16 +0530

    Go to home page

--- frontend/src/NotFound.jsx
+++ frontend/src/NotFound.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+let origin = window.location.origin;
+console.log(origin)
 export const NotFound = () => (
   <div>
     <h1>404 - Not Found!</h1>
-    <Link to="/">
-      Go Home page
-    </Link>
+    <a href={`${window.location.origin}`}>Go to Home Page</a>
   </div>
 );

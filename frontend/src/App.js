e062fba45c202fda6ac2c5d9ecef292365a6a5c9
AnimeshN <animesh.nauti@gmail.com>
2021-08-10 15:11:28 +0530

    switch added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import 'antd/dist/antd.css';
 import 'bootstrap/dist/css/bootstrap.min.css';   
 import { Dropdown } from "./components/Dropdown/Dropdown";
 import { NotFound } from "./NotFound";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import "./components/Navbar/navbar.css";
 
 
@@ -35,19 +35,21 @@ return(<>
 		{/* </div> */}
 	</div>
 	<Router  basename={'/dashboard'}>
-		<Route exact path="/">
-			<Dropdown/>
-		</Route>
-		<Route exact path="/:queryLifecycle">
-			<Dropdown/>
-		</Route>
-		<Route exact path="/:queryLifecycle/:queryCategory">
-			<Dropdown/>
-		</Route>
-		<Route exact path="/:queryLifecycle/:queryCategory/:queryIndicator">
-			<Dropdown/>
-		</Route>
-		<Route component={NotFound} />
+		<Switch>
+			<Route exact path="/">
+				<Dropdown/>
+			</Route>
+			<Route exact path="/:queryLifecycle">
+				<Dropdown/>
+			</Route>
+			<Route exact path="/:queryLifecycle/:queryCategory">
+				<Dropdown/>
+			</Route>
+			<Route exact path="/:queryLifecycle/:queryCategory/:queryIndicator">
+				<Dropdown/>
+			</Route>
+			<Route component={NotFound} />
+		</Switch>
 	</Router>   
 
 

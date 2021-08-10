94f38d885c74e064cb4946481b6421f76dacaf96
AnimeshN <animesh.nauti@gmail.com>
2021-08-10 18:30:42 +0530

    style 404 page

--- frontend/src/NotFound.jsx
+++ frontend/src/NotFound.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 let origin = window.location.origin;
-console.log(origin)
 export const NotFound = () => (
-  <div>
-    <h1>404 - Not Found!</h1>
-    <a href={`${window.location.origin}`}>Go to Home Page</a>
-  </div>
+
+<div id="content" style={{margin:'auto',height:"100vh"}}>
+<div>
+       <p><br></br></p>  
+       <p><br></br></p>   
+       
+          <h1 class="error-page-title">404</h1>
+          <p>Page not found.</p>
+          <h4>Please use the <a        
+            href={`${window.location.origin}`}>Home Page</a></h4>
+
+         <p><br></br></p>
+         <p><br></br></p>
+ 
+</div>
+ 
+</div>
+
+  
 );

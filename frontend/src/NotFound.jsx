d2cc33a2c246c9f2ed7161e83bf4859ddd14ac62
vidya85 <sanapvidya4u@gmail.com>
2021-08-23 19:02:57 +0530

    '404errorpage'

--- frontend/src/NotFound.jsx
+++ frontend/src/NotFound.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 let origin = window.location.origin;
-export const NotFound = () => (
-
+export const NotFound = ({httpStatusCode, httpStatusMsg}) => (
 <div id="content" style={{margin:'auto',height:"100vh"}}>
 <div>
        <p><br></br></p>  
        <p><br></br></p>   
        
-          <h1 class="error-page-title">404</h1>
-          <p>Page not found.</p>
-          <h4>Please use the <a        
+          <h4 class="error-page-title" style={{fontSize:'30px'}}>{httpStatusCode} Page not found error</h4>
+          <p>{httpStatusMsg}</p>
+          <h4>Please use the <a   style={{color:'blue'}}     
             href={`${window.location.origin}`}>Home Page</a></h4>
 
          <p><br></br></p>

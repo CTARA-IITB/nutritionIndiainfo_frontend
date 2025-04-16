2d619ac4f3df85db5ee057883b4851dff2cb8d9d
AnimeshN <nautiyalanimesh@gmail.com>
2025-04-16 18:38:35 +0530

    updated X and added linked icon

--- src/components/Share/ShareImage.jsx
+++ src/components/Share/ShareImage.jsx
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from "react";
 import "./Share.css";
-import { WhatsappIcon,WhatsappShareButton,TwitterIcon,TwitterShareButton,FacebookIcon,FacebookShareButton} from 'react-share';
+import { FaXTwitter } from 'react-icons/fa6'
+import { WhatsappIcon,WhatsappShareButton,TwitterIcon,TwitterShareButton,FacebookIcon,FacebookShareButton,LinkedinIcon,LinkedinShareButton,XIcon} from 'react-share';
 
 const ShareImage =({title,selLifecycle,selCategory,selIndicator})=> {
   const [url,setUrl]= useState();
@@ -10,7 +11,8 @@ const ShareImage =({title,selLifecycle,selCategory,selIndicator})=> {
   },[])
   return (
     <div className="container"  >
-      <TwitterShareButton title={title} url={url}> <TwitterIcon  size={35} round={true}/></TwitterShareButton> 
+      <TwitterShareButton title={title} url={url}>  <FaXTwitter size={35} style={{ borderRadius: '50%' }} /></TwitterShareButton> 
+      <LinkedinShareButton title={title} url={url}> <LinkedinIcon  size={35} round={true}/></LinkedinShareButton> 
       <WhatsappShareButton title={title}  url={url}> <WhatsappIcon size={35} round={true}/></WhatsappShareButton>
       <FacebookShareButton title={title}  url={url}> <FacebookIcon size={35} round={true}/> </FacebookShareButton>
     </div>
